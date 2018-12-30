94e5023f1ea3f48393d3d59ef612cc5e3fbe6233
Jon Yu <jon.yu.1105@gmail.com>
2018-12-30 15:06:21 -0500

    Created MyNetworkManager (inheriting from NetworkManager) to allow easier access to NetworkManager methods for custom UI.

---
+++ Ahoy Matey/Assets/Network/MyNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class MyNetworkManager : NetworkManager {
+
+    //private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+        //networkManager = GetComponent<NetworkManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost() {
+        Debug.Log("Starting Host: " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost() {
+        Debug.Log("Host started at: " + Time.timeSinceLevelLoad);
+    }
+
+}
