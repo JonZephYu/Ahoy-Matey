152337b02ada64517c8e207ff194f6ebf6712bcc
Jon Yu <jon.yu.1105@gmail.com>
2018-12-31 15:02:09 -0500

    Set up log messages for server start and client start/connect.

--- Ahoy Matey/Assets/Network/MyNetworkManager.cs
+++ Ahoy Matey/Assets/Network/MyNetworkManager.cs
@@ -19,12 +19,26 @@ public class MyNetworkManager : NetworkManager {
 	}
 
     public void MyStartHost() {
-        Debug.Log("Starting Host: " + Time.timeSinceLevelLoad);
+        Debug.Log(Time.timeSinceLevelLoad + " Starting Host.");
         StartHost();
     }
 
     public override void OnStartHost() {
-        Debug.Log("Host started at: " + Time.timeSinceLevelLoad);
+        Debug.Log(Time.timeSinceLevelLoad + " Host started.");
+    }
+
+    public override void OnStartClient(NetworkClient myClient) {
+        Debug.Log(Time.timeSinceLevelLoad + " Client started request.");
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn) {
+        Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots() {
+        Debug.Log(".");
     }
 
 }
